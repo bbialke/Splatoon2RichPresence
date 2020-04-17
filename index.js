e453ac190176acc8627bdf0eddc3c9d72e6bc8db
bbialke <bradbialke05@gmail.com>
2020-04-17 15:51:59 -0700

    Added a layout and basic styling

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ let mainWindow;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1280,
+    height: 720,
     resizable: false,
     titleBarStyle: 'hidden',
     webPreferences: {
